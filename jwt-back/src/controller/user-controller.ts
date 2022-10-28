ec95ee0dbc4613b55e16941556e2c01269f7ffbb
Pavel Shmakov <pavel.shmakov@zyfra.com>
2022-10-28 09:56:54 +0700

    before install class-validator

--- jwt-back/src/controller/user-controller.ts
+++ jwt-back/src/controller/user-controller.ts
@@ -2,25 +2,34 @@ import {
   Controller,
   Get,
   Param,
+  Body,
   UseBefore,
   UseAfter,
   UseInterceptor,
   Action,
+  Post,
+  OnUndefined,
 } from 'routing-controllers';
 import { loggingBefore, loggingAfter } from '../middleware/middleware';
 import 'reflect-metadata';
 
 @Controller()
-@UseBefore(loggingBefore)
-@UseAfter(loggingAfter)
-@UseInterceptor(function (action: Action, context: any): any {
-  console.log('change response ...');
-  context = 'interceptor';
-  return context;
-})
+// @UseBefore(loggingBefore)
+// @UseAfter(loggingAfter)
+// @UseInterceptor(function (action: Action, context: any): any {
+//   console.log('change response ...');
+//   context = 'interceptor';
+//   return context;
+// })
 export class UserController {
   @Get('/users/:id')
   getOne(@Param('id') id: number) {
     return `This action returns user #${id}`;
+  }
+
+  @Post('/users/:id')
+  @OnUndefined(204)
+  postOne(@Param('id') id: number, @Body() info: any): void {
+    console.log(JSON.stringify(info))
   }
 }
---
+++ jwt-back/src/model/inofo.ts
@@ -0,0 +1,1 @@
+import { IsDefined } from 'class-va'
